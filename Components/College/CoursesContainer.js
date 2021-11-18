fb255c1ef03e2875a60a2405667354d7def83632
reactexcel <arun.etech2011@gmail.com>
2021-11-18 18:56:24 +0530

    rating completed

--- Components/College/CoursesContainer.js
+++ Components/College/CoursesContainer.js
@@ -13,8 +13,9 @@ import { downArrow } from "../Common/Images";
 import CoursesList from './CoursesList';
 import { faqIcon } from "../Common/Images"
 import Faqs from "./Faqs";
-import { ratingIcon } from "../Common/Images"
-
+import { ratingIcon } from "../Common/Images";
+import { ratedStar } from '../Common/Images';
+import { blankStar } from '../Common/Images';
 
 const Programmes = () => {
     return (
@@ -24,6 +25,30 @@ const Programmes = () => {
     );
 }
 const CoursesContainer = () => {
+    let totalStar = [1, 2, 3, 4, 5];
+    let ratingData = [
+        {
+            name: "Placements",
+            rating: 3
+        },
+        {
+            name: "Infrastructure",
+            rating: 2
+        },
+        {
+            name: "Faculty & Course Curriculum",
+            rating: 5
+        },
+        {
+            name: "Crowd & Campus Life",
+            rating: 1
+        },
+        {
+            name: "Value for Money",
+            rating: 2
+        },
+    ];
+    let selectedStar = 4;
     return (
         <div>
             <div className="grid grid-cols-4 mt-5 gap-x-3">
@@ -53,22 +78,82 @@ const CoursesContainer = () => {
                 </div>
             </div>
             <Faqs />
-            <div className="border">
+            <div className="mt-8">
                 <div className="flex justify-center items-center">
-              <img src={ ratingIcon} alt="ratingIcon" height="24px" width="24px" className="p-0.5 mr-2.5 border border-blue-550 rounded-full"/>
-            <p className="text-blue-550 text-2xl"> COLLEGE RATING
-            </p>
+                    <img src={ratingIcon} alt="ratingIcon" height="24px" width="24px" className="p-0.5 mr-2.5 border border-blue-550 rounded-full" />
+                    <p className="text-blue-550 text-2xl"> COLLEGE RATING
+                    </p>
                 </div>
-           <div className="grid grid-cols-7 mt-6">
-                <div className="col-span-2">
-                    <p>Overall Rating (Out of 5)
-                       </p>
+                <div className="grid grid-cols-7 mt-16">
+
+
+
+                    <div className="col-span-2">
+                        <p className=" text-blue-550">Overall Rating <span className="text-gray-450"> (Out of 5) </span>
+                        </p>
+
+                        <div className="flex mt-11 ml-3.5">
+                            {
+                                [1, 2, 3, 4, 5].map((value, index) => {
+                                    if (selectedStar >= value) {
+                                        return (
+                                            <img src={ratedStar} alt="ratedStar" />
+                                        )
+                                    }
+                                    else {
+                                        return (
+                                            <img src={blankStar} alt="not rated star"/>
+                                        )
+                                    }
+                                })
+                            }
+                        </div>
+                        <p className=" mt-4 ml-11"> ({selectedStar}) Review</p>
+                    </div>
+
+                    <div className="col-span-5">
+                        <p className="text-blue-550 ml-5">Component Ratings <span className="text-gray-450"> (Out of 5) </span></p>
+                        <div className="grid grid-cols-6 mt-7">
+                            <div className="col-span-2">
+                                {
+                                    ratingData.map((value, index) => {
+                                        return (
+                                            <p className="my-6 text-base text-gray-450">{value.name}</p>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="col-span-2">
+                                {
+                                    ratingData.map((value, index) => {
+                                        return (
+                                            <div key={index} className="flex">
+                                                {[1, 2, 3, 4, 5].map((notRated, i) => {
+                                                    return (
+                                                        <div key={i}>
+                                                            <img src={value.rating >= notRated ? ratedStar : blankStar} alt="rated star" className="my-3" />
+                                                        </div>
+                                                    )
+                                                })}
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className="col-span-2 text-gray-450 text-sm">
+                                The Verified badge indicates that the reviewer's details have been verified by Stulink,
+                                and reviewers are bona fide students of this college. These reviews and ratings have been
+                                given by students. Stulink does not endorsed the same. Out of 276 published reviews, 253 reviews
+                                are verified.
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+
                 </div>
-                <div className="col-span-5">
-                  <p>Component Ratings (Out of 5)</p>
-                </div>
-               
-               </div> 
             </div>
         </div>
     );
